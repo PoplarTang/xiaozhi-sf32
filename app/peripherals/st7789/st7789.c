8993f7fb23b0910bc48d058bbabd6267a86dab1d
poplartang <poplar_tang@163.com>
2025-08-28 10:34:14 +0800

    更新st7789驱动

--- app/peripherals/st7789/st7789.c
+++ app/peripherals/st7789/st7789.c
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * @file   st7789h2.c
+  * @file   st7789v.c
   * @author Sifli software development team
-  * @brief   This file includes the LCD driver for ST7789 LCD.
+  * @brief   This file includes the LCD driver for st7789v LCD.
   * @attention
   ******************************************************************************
 */
@@ -68,19 +68,25 @@
 #endif
 
 
+
 /**
-  * @brief ST7789 chip IDs
+  * @brief ST7789V chip IDs
   */
 #define THE_LCD_ID                  0x85
 
 /**
-  * @brief  ST7789 Size
+  * @brief  ST7789V Size
   */
-// #define  THE_LCD_PIXEL_WIDTH    ((uint16_t)240)
-// #define  THE_LCD_PIXEL_HEIGHT   ((uint16_t)280)
+#define  THE_LCD_PIXEL_WIDTH    ((uint16_t)240)
+#define  THE_LCD_PIXEL_HEIGHT   ((uint16_t)280)
+
+
+
+
+
 
 /**
-  * @brief  ST7789 Registers
+  * @brief  ST7789V Registers
   */
 #define REG_LCD_ID             0x04
 #define REG_SLEEP_IN           0x10
@@ -100,21 +106,29 @@
 #define REG_IDLE_MODE_OFF      0x38
 #define REG_IDLE_MODE_ON       0x39
 #define REG_COLOR_MODE         0x3A
-#define REG_WBRIGHT            0x51 /* Write brightness*/
+#define REG_WBRIGHT            0x51
 
 #define REG_PORCH_CTRL         0xB2
-#define ST7789_FRAME_CTRL      0xB3
+
 #define REG_GATE_CTRL          0xB7
 #define REG_VCOM_SET           0xBB
 #define REG_LCM_CTRL           0xC0
 #define REG_VDV_VRH_EN         0xC2
-#define REG_VRH_SET            0xC3
 #define REG_VDV_SET            0xC4
 
 #define REG_FR_CTRL            0xC6
 #define REG_POWER_CTRL         0xD0
 #define REG_PV_GAMMA_CTRL      0xE0
 #define REG_NV_GAMMA_CTRL      0xE1
+#define REG_SPI2EN             0xE7
+
+
+
+
+
+
+
+
 
 //#define DEBUG
 
@@ -123,20 +137,27 @@
 #else
     #define DEBUG_PRINTF(...)
 #endif
+
 static void LCD_WriteReg(LCDC_HandleTypeDef *hlcdc, uint16_t LCD_Reg, uint8_t *Parameters, uint32_t NbParameters);
 static uint32_t LCD_ReadData(LCDC_HandleTypeDef *hlcdc, uint16_t RegValue, uint8_t ReadSize);
 
+
+
+
+
 static LCDC_InitTypeDef lcdc_int_cfg =
 {
-    .lcd_itf = LCDC_INTF_SPI_DCX_1DATA,
+    .lcd_itf = LCDC_INTF_SPI_NODCX_2DATA,
     .freq = 24000000,
     .color_mode = LCDC_PIXEL_FORMAT_RGB565,
+
     .cfg = {
         .spi = {
-
-            .dummy_clock = 1,
-            .syn_mode = HAL_LCDC_SYNC_DISABLE,
+            .dummy_clock = 0,
+            .syn_mode = HAL_LCDC_SYNC_VER,
             .vsyn_polarity = 0,
+            //default_vbp=2, frame rate=82, delay=115us,
+            //TODO: use us to define delay instead of cycle, delay_cycle=115*48
             .vsyn_delay_us = 1000,
             .hsyn_num = 0,
         },
@@ -144,7 +165,12 @@ static LCDC_InitTypeDef lcdc_int_cfg =
 
 };
 
-extern void BSP_LCD_BLKSet(void);
+
+
+
+
+
+
 
 /**
   * @brief  spi read/write mode
@@ -167,9 +193,6 @@ static void LCD_ReadMode(LCDC_HandleTypeDef *hlcdc, bool enable)
     }
 }
 
-#define LCD_CMD_MV_BIT       (1 << 5) // Row/Column order, 0: normal mode, 1: reverse mode
-#define LCD_CMD_MX_BIT       (1 << 6) // Column address order, 0: left to right, 1: right to left
-#define LCD_CMD_MY_BIT       (1 << 7) // Row address order, 0: top to bottom, 1: bottom to top
 /**
   * @brief  Power on the LCD.
   * @param  None
@@ -179,50 +202,71 @@ static void LCD_Init(LCDC_HandleTypeDef *hlcdc)
 {
     uint8_t   parameter[14];
 
-    rt_kprintf("DBG:%s %d",__FUNCTION__, __LINE__);
-    /* Initialize ST7789 low level bus layer ----------------------------------*/
+    /* Initialize ST7789V low level bus layer ----------------------------------*/
     memcpy(&hlcdc->Init, &lcdc_int_cfg, sizeof(LCDC_InitTypeDef));
     HAL_LCDC_Init(hlcdc);
 
-    LCD_DRIVER_DELAY_MS(25);
     BSP_LCD_Reset(0);//Reset LCD
-    LCD_DRIVER_DELAY_MS(25);
+    HAL_Delay_us(10);
     BSP_LCD_Reset(1);
 
-    LCD_DRIVER_DELAY_MS(50);
-    // BSP_LCD_PowerUp();
-    BSP_LCD_BLKSet();
+    /* Sleep In Command */
+    LCD_WriteReg(hlcdc, REG_SLEEP_IN, (uint8_t *)NULL, 0);
+    /* Wait for 10ms */
+    LCD_DRIVER_DELAY_MS(10);
 
-    // /* SLP OUT 0x11*/
-    // LCD_WriteReg(hlcdc, REG_SLEEP_OUT, (uint8_t *)NULL, 0);
-
-    // /* Sleep In Command */
-    // LCD_WriteReg(hlcdc, REG_SLEEP_IN, (uint8_t *)NULL, 0);
-    // /* Wait for 10ms */
-    // LCD_DRIVER_DELAY_MS(10);
-
-    // /* SW Reset Command */
-    // LCD_WriteReg(hlcdc, 0x01, (uint8_t *)NULL, 0);
-    // /* Wait for 200ms */
-    // LCD_DRIVER_DELAY_MS(200);
+    /* SW Reset Command */
+    LCD_WriteReg(hlcdc, 0x01, (uint8_t *)NULL, 0);
+    /* Wait for 200ms */
+    LCD_DRIVER_DELAY_MS(200);
 
     /* Sleep Out Command */
     LCD_WriteReg(hlcdc, REG_SLEEP_OUT, (uint8_t *)NULL, 0);
     /* Wait for 120ms */
     LCD_DRIVER_DELAY_MS(120);
 
-    /* Normal display for Driver Down side - MADCTL (0x36) */
+    /* Normal display for Driver Down side */
     parameter[0] = 0x00;
     LCD_WriteReg(hlcdc, REG_NORMAL_DISPLAY, parameter, 1);
 
-    /* Color mode 16bits/pixel - COLMOD (0x3A) */
-    parameter[0] = 0x65;
+    /* Color mode 16bits/pixel */
+    parameter[0] = 0x55;
     LCD_WriteReg(hlcdc, REG_COLOR_MODE, parameter, 1);
 
-    /* Display inversion On - INVON (0x21) */
-    // LCD_WriteReg(hlcdc, REG_DISPLAY_INVERSION, (uint8_t *)NULL, 0);
-    
-    /* PORCH control setting - (0xB2) */
+    /* Display inversion On */
+    LCD_WriteReg(hlcdc, REG_DISPLAY_INVERSION, (uint8_t *)NULL, 0);
+
+#if 0
+    /* set LCD RAM buffer to black */
+    /* Set Column address CASET */
+    parameter[0] = 0x00;
+    parameter[1] = 0x00;
+    parameter[2] = 0x00;
+    parameter[3] = THE_LCD_PIXEL_WIDTH - 1;
+    LCD_WriteReg(hlcdc, REG_CASET, parameter, 4);
+    /* Set Row address RASET */
+    parameter[0] = 0x00;
+    parameter[1] = ROW_OFFSET;
+    parameter[2] = ((THE_LCD_PIXEL_HEIGHT + ROW_OFFSET - 1) >> 8) & 0xFF;
+    parameter[3] = ((THE_LCD_PIXEL_HEIGHT + ROW_OFFSET - 1) & 0xFF);
+    LCD_WriteReg(hlcdc, REG_RASET, parameter, 4);
+
+    LCD_WriteReg(hlcdc, REG_WRITE_RAM, (uint8_t *)NULL, 0);
+
+    for (uint32_t i = 0; i < THE_LCD_PIXEL_WIDTH * THE_LCD_PIXEL_WIDTH; i++)
+    {
+        LCD_IO_WriteData16(0);
+    }
+#endif
+
+    /* Enable SPI 2 data lane mode */
+    parameter[0] = 0x10;
+    LCD_WriteReg(hlcdc, REG_SPI2EN, parameter, 1);
+
+#if 0
+
+    /*--------------- ST7789V Frame rate setting -------------------------------*/
+    /* PORCH control setting */
     parameter[0] = 0x0C;
     parameter[1] = 0x0C;
     parameter[2] = 0x00;
@@ -230,96 +274,152 @@ static void LCD_Init(LCDC_HandleTypeDef *hlcdc)
     parameter[4] = 0x33;
     LCD_WriteReg(hlcdc, REG_PORCH_CTRL, parameter, 5);
 
-    /* Gate Control (0xB7) */
-    parameter[0] = 0x72;
+    /* GATE control setting */
+    parameter[0] = 0x35;
     LCD_WriteReg(hlcdc, REG_GATE_CTRL, parameter, 1);
 
-    /* VCOM setting (0xBB) */
-    parameter[0] = 0x3D;
+    /*--------------- ST7789V Power setting ------------------------------------*/
+    /* VCOM setting */
+    parameter[0] = 0x1F;
     LCD_WriteReg(hlcdc, REG_VCOM_SET, parameter, 1);
 
-    /* LCM Control (0xC0) */
+    /* LCM Control setting */
     parameter[0] = 0x2C;
     LCD_WriteReg(hlcdc, REG_LCM_CTRL, parameter, 1);
 
-    /* VDV and VRH Command Enable (0xC2) */
+    /* VDV and VRH Command Enable */
     parameter[0] = 0x01;
-    LCD_WriteReg(hlcdc, REG_VDV_VRH_EN, parameter, 1);
+    parameter[1] = 0xC3;
+    LCD_WriteReg(hlcdc, REG_VDV_VRH_EN, parameter, 2);
 
-    /* VRH 0xC3 */
-    parameter[0] = 0x19;
-    LCD_WriteReg(hlcdc, REG_VRH_SET, parameter, 1);
-
-    /* VDV Set (0xC4) */
+    /* VDV Set */
     parameter[0] = 0x20;
     LCD_WriteReg(hlcdc, REG_VDV_SET, parameter, 1);
 
-    /* Frame Rate Control in normal mode (0xC6) */
-    parameter[0] = 0x01;
+    /* Frame Rate Control in normal mode */
+    parameter[0] = 0x0F;
     LCD_WriteReg(hlcdc, REG_FR_CTRL, parameter, 1);
 
-    /* Power Control (0xD0) */
+    /* Power Control */
     parameter[0] = 0xA4;
     parameter[1] = 0xA1;
-    LCD_WriteReg(hlcdc, REG_POWER_CTRL, parameter, 2);
+    LCD_WriteReg(hlcdc, REG_POWER_CTRL, parameter, 1);
 
-    /*--------------- ST7789 Gamma setting ------------------------------------*/
-    /* Positive Voltage Gamma Control (0xE0) */
-    parameter[0] = 0xD0;
-    parameter[1] = 0x08;
-    parameter[2] = 0x0E;
+#endif
+
+
+    //TODO: configuration can be different though the LCD chip is same
+#ifdef LCD_USING_ROUND_TYPE1
+
+    /*--------------- ST7789V Gamma setting ------------------------------------*/
+    /* Positive Voltage Gamma Control */
+    parameter[0] = 0x70;
+    parameter[1] = 0x04;
+    parameter[2] = 0x08;
     parameter[3] = 0x09;
     parameter[4] = 0x09;
     parameter[5] = 0x05;
-    parameter[6] = 0x31;
+    parameter[6] = 0x2A;
     parameter[7] = 0x33;
-    parameter[8] = 0x48;
-    parameter[9] = 0x17;
-    parameter[10] = 0x14;
-    parameter[11] = 0x15;
-    parameter[12] = 0x31;
-    parameter[13] = 0x34;
+    parameter[8] = 0x41;
+    parameter[9] = 0x07;
+    parameter[10] = 0x13;
+    parameter[11] = 0x13;
+    parameter[12] = 0x29;
+    parameter[13] = 0x2F;
     LCD_WriteReg(hlcdc, REG_PV_GAMMA_CTRL, parameter, 14);
 
-    /* Negative Voltage Gamma Control (0xE1) */
-    parameter[0] = 0xD0;
-    parameter[1] = 0x08;
-    parameter[2] = 0x0E;
-    parameter[3] = 0x09;
+    /* Negative Voltage Gamma Control */
+    parameter[0] = 0x70;
+    parameter[1] = 0x03;
+    parameter[2] = 0x09;
+    parameter[3] = 0x0A;
     parameter[4] = 0x09;
-    parameter[5] = 0x15;
-    parameter[6] = 0x31;
-    parameter[7] = 0x33;
-    parameter[8] = 0x48;
-    parameter[9] = 0x17;
-    parameter[10] = 0x14;
-    parameter[11] = 0x15;
-    parameter[12] = 0x31;
-    parameter[13] = 0x34;
+    parameter[5] = 0x06;
+    parameter[6] = 0x2B;
+    parameter[7] = 0x34;
+    parameter[8] = 0x41;
+    parameter[9] = 0x07;
+    parameter[10] = 0x12;
+    parameter[11] = 0x14;
+    parameter[12] = 0x28;
+    parameter[13] = 0x2E;
     LCD_WriteReg(hlcdc, REG_NV_GAMMA_CTRL, parameter, 14);
 
-    /* Set frame control (0xB3)
-    parameter[0] = 0x01;
-    parameter[1] = 0x0F;
-    parameter[2] = 0x0F;
-    LCD_WriteReg(hlcdc, ST7789_FRAME_CTRL, parameter, 3);
-     */
-    
+#else
 
-    /* INVERSE ON 0x21*/
-    LCD_WriteReg(hlcdc, REG_DISPLAY_INVERSION, (uint8_t *)NULL, 0);
-    /* SLP OUT 0x11*/
-    LCD_WriteReg(hlcdc, REG_SLEEP_OUT, (uint8_t *)NULL, 0);
-    /* normal ON (0x13)*/
-    LCD_WriteReg(hlcdc, 0x13, (uint8_t *)NULL, 0);
-    /* Display ON command (0x29) */
+    /*--------------- ST7789V Gamma setting ------------------------------------*/
+    /* Positive Voltage Gamma Control */
+    parameter[0] = 0xD0;
+    parameter[1] = 0x13;
+    parameter[2] = 0x1A;
+    parameter[3] = 0x0A;
+    parameter[4] = 0x0A;
+    parameter[5] = 0x26;
+    parameter[6] = 0x3F;
+    parameter[7] = 0x54;
+    parameter[8] = 0x54;
+    parameter[9] = 0x18;
+    parameter[10] = 0x14;
+    parameter[11] = 0x14;
+    parameter[12] = 0x30;
+    parameter[13] = 0x33;
+    LCD_WriteReg(hlcdc, REG_PV_GAMMA_CTRL, parameter, 14);
+
+    /* Negative Voltage Gamma Control */
+    parameter[0] = 0xD0;
+    parameter[1] = 0x13;
+    parameter[2] = 0x1A;
+    parameter[3] = 0x0A;
+    parameter[4] = 0x0A;
+    parameter[5] = 0x26;
+    parameter[6] = 0x3F;
+    parameter[7] = 0x54;
+    parameter[8] = 0x54;
+    parameter[9] = 0x1A;
+    parameter[10] = 0x16;
+    parameter[11] = 0x16;
+    parameter[12] = 0x32;
+    parameter[13] = 0x35;
+    LCD_WriteReg(hlcdc, REG_NV_GAMMA_CTRL, parameter, 14);
+#endif
+
+#if 0
+
+    /* Set Column address CASET */
+    parameter[0] = 0x00;
+    parameter[1] = 0x00;
+    parameter[2] = 0x00;
+    parameter[3] = THE_LCD_PIXEL_WIDTH - 1;
+    LCD_WriteReg(hlcdc, REG_CASET, parameter, 4);
+    /* Set Row address RASET */
+    parameter[0] = 0x00;
+    parameter[1] = ROW_OFFSET;
+    parameter[2] = ((THE_LCD_PIXEL_HEIGHT + ROW_OFFSET - 1) >> 8) & 0xFF;
+    parameter[3] = ((THE_LCD_PIXEL_HEIGHT + ROW_OFFSET - 1) & 0xFF);
+    LCD_WriteReg(hlcdc, REG_RASET, parameter, 4);
+
+    LCD_WriteReg(hlcdc, REG_WRITE_RAM, (uint8_t *)NULL, 0);
+
+
+    hwp_lcdc->CANVAS_BG = (0xFF << LCD_IF_CANVAS_BG_RED_Pos) | (0 << LCD_IF_CANVAS_BG_GREEN_Pos) | (0 << LCD_IF_CANVAS_BG_BLUE_Pos);
+    hwp_lcdc->CANVAS_TL_POS = (0    << LCD_IF_CANVAS_TL_POS_X0_Pos) | (0  << LCD_IF_CANVAS_TL_POS_Y0_Pos);
+    hwp_lcdc->CANVAS_BR_POS = (239  << LCD_IF_CANVAS_BR_POS_X1_Pos) | ((239) << LCD_IF_CANVAS_BR_POS_Y1_Pos);
+
+    hwp_lcdc->COMMAND = 0x1;
+#endif
+
+    /* Set frame control, refresh rate: 82Hz*/
+    parameter[0] = 0x7;
+    LCD_WriteReg(hlcdc, REG_FR_CTRL, parameter, 1);
+
+    /* Tearing Effect Line On: Option (00h:VSYNC Only, 01h:VSYNC & HSYNC ) */
+    parameter[0] = 0x00;
+    LCD_WriteReg(hlcdc, REG_TEARING_EFFECT, parameter, 1);
+
+    /* Display ON command */
     LCD_WriteReg(hlcdc, REG_DISPLAY_ON, (uint8_t *)NULL, 0);
 
-    /* Tearing Effect Line On: Option (00h:VSYNC Only, 01h:VSYNC & HSYNC) (0x35) */
-    // parameter[0] = 0x00;
-    // LCD_WriteReg(hlcdc, REG_TEARING_EFFECT, parameter, 1);
-    
-    LCD_DRIVER_DELAY_MS(50);
 }
 
 /**
@@ -331,8 +431,9 @@ static uint32_t LCD_ReadID(LCDC_HandleTypeDef *hlcdc)
 {
     uint32_t data;
 
-    data = LCD_ReadData(hlcdc, REG_LCD_ID, 3);
-    rt_kprintf("DBG:%s %d ID:%0x",__FUNCTION__, __LINE__,data);
+    data = LCD_ReadData(hlcdc, REG_LCD_ID, 4);
+    data = ((data << 1) >> 8) & 0xFFFFFF;
+
     return data;
 }
 
@@ -343,7 +444,6 @@ static uint32_t LCD_ReadID(LCDC_HandleTypeDef *hlcdc)
   */
 static void LCD_DisplayOn(LCDC_HandleTypeDef *hlcdc)
 {
-    rt_kprintf("DBG:%s %d",__FUNCTION__, __LINE__);
     /* Display On */
     LCD_WriteReg(hlcdc, REG_DISPLAY_ON, (uint8_t *)NULL, 0);
 }
@@ -355,30 +455,18 @@ static void LCD_DisplayOn(LCDC_HandleTypeDef *hlcdc)
   */
 static void LCD_DisplayOff(LCDC_HandleTypeDef *hlcdc)
 {
-    rt_kprintf("DBG:%s %d",__FUNCTION__, __LINE__);
     /* Display Off */
     LCD_WriteReg(hlcdc, REG_DISPLAY_OFF, (uint8_t *)NULL, 0);
 }
 
-
-/**
- * @brief Set LCD & LCDC clip area
- * @param hlcdc LCD controller handle
- * @param Xpos0 - Clip area left coordinate, base on LCD top-left, same as below.
- * @param Ypos0 - Clip area top coordinate
- * @param Xpos1 - Clip area right coordinate
- * @param Ypos1 - Clip area bottom coordinate
- */
 static void LCD_SetRegion(LCDC_HandleTypeDef *hlcdc, uint16_t Xpos0, uint16_t Ypos0, uint16_t Xpos1, uint16_t Ypos1)
 {
     uint8_t   parameter[4];
 
-    //Set LCDC clip area
     HAL_LCDC_SetROIArea(hlcdc, Xpos0, Ypos0, Xpos1, Ypos1);
 
-    //Set LCD clip area
-    Xpos0 += ROW_OFFSET;
-    Xpos1 += ROW_OFFSET;
+    Ypos0 += ROW_OFFSET;
+    Ypos1 += ROW_OFFSET;
 
     parameter[0] = (Xpos0) >> 8;
     parameter[1] = (Xpos0) & 0xFF;
@@ -403,29 +491,20 @@ static void LCD_SetRegion(LCDC_HandleTypeDef *hlcdc, uint16_t Xpos0, uint16_t Yp
 static void LCD_WritePixel(LCDC_HandleTypeDef *hlcdc, uint16_t Xpos, uint16_t Ypos, const uint8_t *RGBCode)
 {
     uint8_t data = 0;
+
     /* Set Cursor */
     LCD_SetRegion(hlcdc, Xpos, Ypos, Xpos, Ypos);
     LCD_WriteReg(hlcdc, REG_WRITE_RAM, (uint8_t *)RGBCode, 2);
 }
 
-/**
- * @brief Send layer data to LCD
- * @param hlcdc LCD controller handle
- * @param RGBCode - Pointer to layer data
- * @param Xpos0 - Layer data left coordinate, base on LCD top-left, same as below.
- * @param Ypos0 - Layer data top coordinate
- * @param Xpos1 - Layer data right coordinate
- * @param Ypos1 - Layer data bottom coordinate
- */
 static void LCD_WriteMultiplePixels(LCDC_HandleTypeDef *hlcdc, const uint8_t *RGBCode, uint16_t Xpos0, uint16_t Ypos0, uint16_t Xpos1, uint16_t Ypos1)
 {
     uint32_t size;
 
-    //Set default layer data
     HAL_LCDC_LayerSetData(hlcdc, HAL_LCDC_LAYER_DEFAULT, (uint8_t *)RGBCode, Xpos0, Ypos0, Xpos1, Ypos1);
-    //Write datas to LCD register
     HAL_LCDC_SendLayerData2Reg_IT(hlcdc, REG_WRITE_RAM, 1);
 }
+
 
 
 /**
@@ -472,13 +551,14 @@ static uint32_t LCD_ReadPixel(LCDC_HandleTypeDef *hlcdc, uint16_t Xpos, uint16_t
     LCD_SetRegion(hlcdc, Xpos, Ypos, Xpos, Ypos);
 
     /*
-        read ram need 8 dummy cycle, and it's result is 24bit color which format is:
+        read ram need 7 dummy cycle, and it's result is 24bit color which format is:
 
         6bit red + 2bit dummy + 6bit green + 2bit dummy + 6bit blue + 2bit dummy
 
     */
     c =  LCD_ReadData(hlcdc, REG_READ_RAM, 4);
-    c >>= lcdc_int_cfg.cfg.spi.dummy_clock; //revert fixed dummy cycle
+    c <<= 1;
+    //c >>= lcdc_int_cfg.dummy_clock; //revert fixed dummy cycle
 
     switch (lcdc_int_cfg.color_mode)
     {
@@ -509,13 +589,13 @@ static uint32_t LCD_ReadPixel(LCDC_HandleTypeDef *hlcdc, uint16_t Xpos, uint16_t
         ret_v = (uint32_t)((c & 0xFCFCFC) | ((c >> 6) & 0x030303));
         break;
 
-
     default:
         RT_ASSERT(0);
         break;
     }
 
-    rt_kprintf("ST7789_ReadPixel %x -> %x\n",c, ret_v);
+    //rt_kprintf("ST7789V_ReadPixel %x -> %x\n",c, ret_v);
+
 
     LCD_WriteReg(hlcdc, REG_COLOR_MODE, parameter, 1);
 
@@ -541,19 +621,6 @@ static void LCD_SetColorMode(LCDC_HandleTypeDef *hlcdc, uint16_t color_mode)
         lcdc_int_cfg.color_mode = LCDC_PIXEL_FORMAT_RGB565;
         break;
 
-    /*
-       pretend that st7789v can support RGB888,
-
-       treated as RGB666 actually(6bit R + 2bit dummy + 6bit G + 2bit dummy + 6bit B + 2bit dummy )
-
-       lcdc NOT support RGB666
-    */
-    case RTGRAPHIC_PIXEL_FORMAT_RGB888:
-        /* Color mode 18bits/pixel */
-        parameter[0] = 0x66;
-        lcdc_int_cfg.color_mode = LCDC_PIXEL_FORMAT_RGB888;
-        break;
-
 
     default:
         RT_ASSERT(0);
@@ -566,32 +633,38 @@ static void LCD_SetColorMode(LCDC_HandleTypeDef *hlcdc, uint16_t color_mode)
     HAL_LCDC_SetOutFormat(hlcdc, lcdc_int_cfg.color_mode);
 }
 
-#define ST7789_BRIGHTNESS_MAX 127
-
 static void LCD_SetBrightness(LCDC_HandleTypeDef *hlcdc, uint8_t br)
 {
-    uint8_t bright = (uint8_t)((int)ST7789_BRIGHTNESS_MAX * br / 100);
-    LCD_WriteReg(hlcdc, REG_WBRIGHT, &bright, 1);
+    uint8_t bright = (uint8_t)((uint16_t)UINT8_MAX * br / 100);
+    LCD_WriteReg(hlcdc, REG_WBRIGHT, &br, 1);
 }
 
-static const LCD_DrvOpsDef ST7789_drv =
+
+
+
+
+static const LCD_DrvOpsDef ST7789V_drv =
 {
     LCD_Init,
     LCD_ReadID,
     LCD_DisplayOn,
     LCD_DisplayOff,
+
     LCD_SetRegion,
     LCD_WritePixel,
     LCD_WriteMultiplePixels,
+
     LCD_ReadPixel,
+
     LCD_SetColorMode,
     LCD_SetBrightness,
     NULL,
     NULL
+
 };
 
-LCD_DRIVER_EXPORT2(st7789h2, 0x8181b3, &lcdc_int_cfg,
-                   &ST7789_drv, 1);
+LCD_DRIVER_EXPORT2(st7789v, 0x858552, &lcdc_int_cfg,
+                   &ST7789V_drv, 1);
 
 
 
